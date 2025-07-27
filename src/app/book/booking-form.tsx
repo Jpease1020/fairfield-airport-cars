2443eb1235160d98893b1bbe2b76a10f5f104366
Justin Pease <justinpease2@gmail.com>
2025-07-26 22:39:02 -0400

    Systematic refactor: Remove direct className usage from booking form - replace with reusable components

--- src/app/book/booking-form.tsx
+++ src/app/book/booking-form.tsx
@@ -245,25 +245,25 @@ function BookingFormContent({ booking }: BookingFormProps) {
   };
 
   return (
-    <div className="booking-form-container">
+    <Container>
       {/* Google Maps Error */}
       {mapsError && (
-        <Container className="booking-form-error-container">
-          <div className="booking-form-error-content">
-            <div className="booking-form-error-icon">
-              <svg className="booking-form-error-svg" fill="currentColor" viewBox="0 0 20 20">
+        <Container>
+          <Container>
+            <Container>
+              <svg fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
-            </div>
-            <div className="booking-form-error-text">
-              <H3 className="booking-form-error-title">
+            </Container>
+            <Container>
+              <H3>
                 Location autocomplete temporarily unavailable
               </H3>
-              <div className="booking-form-error-description">
+              <Container>
                 <Text>You can still fill out the form manually. Location suggestions will be restored shortly.</Text>
-              </div>
-            </div>
-          </div>
+              </Container>
+            </Container>
+          </Container>
         </Container>
       )}
 
@@ -556,7 +556,7 @@ function BookingFormContent({ booking }: BookingFormProps) {
           />
         )}
       </Form>
-    </div>
+    </Container>
   );
 }
 
