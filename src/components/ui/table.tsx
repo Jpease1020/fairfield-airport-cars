2e2b4a1de43a8679a0ec4711201b75b39a425959
Justin Pease <justinpease2@gmail.com>
2025-07-26 22:18:11 -0400

    Continue systematic refactoring: remove CSS variables from UI components (label and table)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -10,7 +10,7 @@ const Table = React.forwardRef<
   <div className="">
     <table
       ref={ref}
-      className={cn('w-full caption-bottom text-sm text-text-primary', className)}
+      className={cn('w-full caption-bottom text-sm', className)}
       {...props}
     />
   </div>
