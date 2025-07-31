3184b82a98f0cba76b53d1a713ea07b02126c14f
Justin Pease <justinpease2@gmail.com>
2025-07-31 15:48:01 -0400

    delete

--- src/design/components/templates/index.ts
+++
@@ -1,8 +0,0 @@
-// Template Components
-// Layer 5 & 6: Page Templates and Patterns
-// Built on top of Grid + Containers + Page Sections
-
-export * from './PageTemplates';
-export * from './PagePatterns';
-export * from './ContentTemplates';
-export * from './MarketingTemplates'; 
