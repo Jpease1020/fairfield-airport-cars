8bb062ee4bd8f854fed16dd53cab3d7a9104a279
Justin Pease <justinpease2@gmail.com>
2025-08-25 08:23:16 -0400

    Update dependencies and refactor utils - unrelated to CMS migration

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,9 +1,3 @@
-import { type ClassValue, clsx } from "clsx";
-
-export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs);
-}
-
 export { getAdminAuth, getAdminDb, adminServices } from './firebase-admin';
 export { db } from './firebase';
 export { testFirebaseConnection } from './firebase-test';
